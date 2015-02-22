7f68e2dca873ccc8985bef02e8826f67b33c6fe7
Odin Hørthe Omdal <odin.omdal@gmail.com>
2015-02-22 22:41:05 +0100

    Add simple tests for the public views

--- fkbeta/fk/tests.py
+++ fkbeta/fk/tests.py
@@ -1,25 +1,32 @@
 # Copyright (c) 2012-2013 Benjamin Bruheim <grolgh@gmail.com>
 # This file is covered by the LGPLv3 or later, read COPYING for details.
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.utils import timezone
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from fk.models import Scheduleitem
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+class WebPageTest(TestCase):
+    fixtures = ['test.yaml']
 
+    def test_video_listing(self):
+        r = self.client.get('/video/')
+        self.assertEqual(['dummy video', 'tech video'],
+                         [v.name for v in r.context['videos']])
+        self.assertContains(r, 'dummy video', count=1)
+        self.assertContains(r, 'tech video', count=1)
+
+    def test_video_detail(self):
+        r = self.client.get('/video/1')
+        self.assertContains(
+            r, 'First broadcast: <i>Jan. 1, 2015, 10 a.m.</i>', count=1)
+        self.assertContains(r, '<h1>Video not available</h1>', count=1)
+        self.assertContains(r, '<a href="/organization/1">NUUG</a>', count=1)
+
+    def test_video_guide(self):
+        sched = Scheduleitem.objects.get(video__name='tech video')
+        sched.starttime = timezone.now()
+        sched.save()
+        r = self.client.get('/guide/')
+        self.assertContains(r, '<li class="event"', count=1)
+        self.assertContains(r, 'tech video', count=1)
