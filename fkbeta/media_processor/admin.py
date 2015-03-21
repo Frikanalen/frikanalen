c857fb9443750857ccba60584e207849bcce2c23
Odin Hørthe Omdal <odin.omdal@gmail.com>
2015-03-21 16:45:27 +0100

    media_processor: Make better django admin overview of Task

--- fkbeta/media_processor/admin.py
+++ fkbeta/media_processor/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from media_processor.models import Task
 
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'source_file', 'target_format', 'status')
+    list_filter = ('status', 'target_format')
+    raw_id_fields = ('source_file',)
+
+
+admin.site.register(Task, TaskAdmin)
