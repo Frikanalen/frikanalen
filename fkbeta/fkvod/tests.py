bb6eebcaf828cb9226d469c81b509191547dfd0b
Petter Reinholdtsen <pere@hungry.com>
2018-11-11 08:52:05 +0100

    Add simple test case for the video on demand pages.

---
+++ fkbeta/fkvod/tests.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2018 Petter Reinholdtsen <pere@hungry.com>
+# This file is covered by the LGPLv3 or later, read COPYING for details.
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+class VodTests(TestCase):
+    fixtures = ['test.yaml']
+
+    def test_video_list(self):
+        response = self.client.get(reverse('vod-video-list'))
+        self.assertContains(response, 'RSS', count=1)
+
+    def test_video_list(self):
+        response = self.client.get(reverse('vod-video-detail', args=[1]))
+        self.assertContains(response, 'Category', count=1)
