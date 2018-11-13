06ef0923d3757992a97ff270287bb7f844deeeeb
Petter Reinholdtsen <pere@hungry.com>
2018-11-13 13:31:43 +0100

    New web page listing member organizations listed in the database.

--- fkbeta/fkprofile/views.py
+++ fkbeta/fkprofile/views.py
@@ -1,7 +1,21 @@
 from django.views.generic.detail import DetailView
+from django.shortcuts import render
 from django.utils import timezone
+from django.utils.translation import ugettext as _
 
+from fk.models import Organization
 from fkprofile.models import Profile
+
+def member_organizations_list(request):
+    members = (Organization.objects
+               .filter(fkmember=True)
+               .order_by('name'))
+    return render(request, 'fkprofile/member_organizations_list.html',
+                  {
+                      'members': members,
+                      'title': _('Member organizations'),
+    })
+
 
 class ProfileDetailView(DetailView):
     model = Profile
