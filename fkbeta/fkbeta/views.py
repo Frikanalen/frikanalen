7ceff4e5349f49484d57e6978c6bf2216ea643a3
Petter Reinholdtsen <pere@hungry.com>
2018-11-15 14:42:25 +0100

    Add HTML page title to front page.

--- fkbeta/fkbeta/views.py
+++ fkbeta/fkbeta/views.py
@@ -4,4 +4,7 @@ from django.views.generic import TemplateView
 
 class Frontpage(TemplateView):
     template_name = "frontpage.html"
-    title = _('Frikanalen')
+    def get_context_data(self, *args, **kwargs):
+	context = super(Frontpage, self).get_context_data(*args, **kwargs)
+	context['title'] = _('TV for alle')
+        return context
