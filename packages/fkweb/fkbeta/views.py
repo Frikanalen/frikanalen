196b6b5e6b838f8ca0f06a6b2a51733fe5874a4d
Odin Hørthe Omdal <odin.omdal@gmail.com>
2020-01-22 21:29:08 +0100

    Show what's playing and editor info on frontpage
    Yes, rather hacky, rather ugly. But now we have it :)

--- packages/fkweb/fkbeta/views.py
+++ packages/fkweb/fkbeta/views.py
@@ -1,5 +1,8 @@
+from django.utils import timezone
 from django.utils.translation import ugettext as _
 from django.views.generic import TemplateView
+
+from fk.models import Scheduleitem
 
 
 class Frontpage(TemplateView):
@@ -7,4 +10,9 @@ class Frontpage(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(Frontpage, self).get_context_data(*args, **kwargs)
         context['title'] = _('TV for alle')
+        current, prev = Scheduleitem.objects.filter(starttime__lt=timezone.now()).order_by("-starttime")[:2]
+        context['curr_scheditem'] = current
+        context['prev_scheditem'] = prev
+        context['next_scheditem'] = Scheduleitem.objects.filter(starttime__gte=timezone.now()).order_by("starttime")[0]
+
         return context
