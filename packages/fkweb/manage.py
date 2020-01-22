94fae8616206a6f4fa9d6b07ec71599e8695586a
Odin Hørthe Omdal <odin.omdal@gmail.com>
2020-01-22 17:54:42 +0100

    Fix tests (use sqlite rather than postgresql)

--- packages/fkweb/manage.py
+++ packages/fkweb/manage.py
@@ -5,7 +5,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fkbeta.settings.local")
+    local = "fkbeta.settings.local"
+    test = "fkbeta.settings.test"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", test if 'test' in sys.argv else local)
 
     from django.core.management import execute_from_command_line
 
