fe40b21a67f8be5aee3d6a2a308fb53a976ac975
Tore Sinding Bekkedal <toresbe@gmail.com>
2020-01-29 18:01:49 +0100

    more miscellaneous work

--- packages/frontend/components/Header.js
+++ packages/frontend/components/Header.js
@@ -1,56 +1,69 @@
 import Link from 'next/link';
+import React, { Component } from 'react';
 import UserAuth from './UserAuth';
 
-const Header = () => (
-    <header>
-      <div id="header-logo">
-        <Link href="/" as="/">
-            <a><img src="/static/frikanalen.png" alt="Frikanalen" /></a>
-        </Link>
-      </div>
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.showLogin = this.showLogin.bind(this);
+        this.state = {
+            show_login: false,
+        }
+    }
 
-      <nav>
-        <div className="headerLinks">
-            <Link href="/schedule" as="/schedule"><a>Sendeplan</a></Link>
-            <Link href="/videos" as="/videos"><a>Arkiv</a></Link>
-            <Link href="/members" as="/members"><a>Medlemmer</a></Link>
-            <Link href="/about" as="/about"><a>Om oss</a></Link>
-        </div>
-        <UserAuth />
-      </nav>
+    showLogin () {
+        this.setState({show_login: true});
+    }
 
-    <style jsx>{`
-        #header-logo {
-            padding: 30px 0;
-        }
+    render () {
+        return (
+            <header>
+              <div id="header-logo">
+                <Link href="/" as="/">
+                    <a><img src="/static/frikanalen.png" alt="Frikanalen" /></a>
+                </Link>
+              </div>
 
-        .headerLinks {
-            padding: 0 30px;
-            background: #535151;
-            display: flex;
-            font-family: 'Roboto', sans-serif;
-            font-size: 20pt;
-        }
+              <nav>
+                <div className="headerLinks">
+                    <Link href="/schedule" as="/schedule"><a>Sendeplan</a></Link>
+                    <Link href="/videos" as="/videos"><a>Arkiv</a></Link>
+                    <Link href="/members" as="/members"><a>Medlemmer</a></Link>
+                    <Link href="/about" as="/about"><a>Om oss</a></Link>
+                    <a onClick={this.showLogin}>Logg inn</a>
+                </div>
+                {this.state.show_login? <UserAuth /> : <></>}
+              </nav>
 
-        .headerLinks>a {
-            margin: 0 10px;
-            text-decoration: none;
-            text-transform: lowercase;
-        }
-        .headerLinks>a:link {
-            color: #ddd;
-        }
-        .headerLinks>a:visited {
-            color: #ddd;
-        }
-        .headerLinks>a:hover {
-            color: white;
-        }
-        .headerLinks>a:active {
-            color: white;
-        }
-        `}</style>
-    </header>
-    );
+            <style jsx>{`
+                #header-logo {
+                    padding: 30px 0;
+                }
+
+                .headerLinks {
+                    padding: 0 30px;
+                    background: #535151;
+                    display: flex;
+                    font-family: 'Roboto', sans-serif;
+                    font-size: 20pt;
+                }
+
+                .headerLinks>a {
+                    margin: 0 10px;
+                    text-decoration: none;
+                    text-transform: lowercase;
+                    color: #ddd;
+                }
+                .headerLinks>a:hover {
+                    color: white;
+                }
+                .headerLinks>a:active {
+                    color: white;
+                }
+                `}</style>
+            </header>
+        );
+    }
+}
 
 export default Header;
--- packages/frontend/components/UserAuth.js
+++ packages/frontend/components/UserAuth.js
@@ -12,24 +12,43 @@ class UserAuth extends Component {
         this.password = React.createRef();
         this.state = {
             token: cookies.get('token') || null,
-            username: ''
         };
+    }
+
+    componentDidMount() {
+        if(this.state.token) {
+            this.load_profile_data();
+        }
     }
 
     logged_in_nav = () => {
         return (
-            <div>
+            <div className="user_nav">
+            <div><span className="material-icons">account_box</span>{this.state.email}</div>
             <button onClick={this.handle_logout}>logg ut</button>
             <style jsx>{`
+            .user_nav {
+                display: flex;
+                flex-wrap: no-wrap;
+                font-size: 12pt;
+            }
             button {
+                font-size: calc(inherit - 2pt);
+                color: inherit;
+                font-family: inherit;
+
                 margin: 1px;
                 margin-left: 30px;
-                color: white;
                 background: #333;
                 border: 1px solid white;
-                text-color: white;
-                font-family: 'Roboto', sans-serif;
+            }
+            div {
+                vertical-align: middle !important;
+            }
+            .material-icons {
                 font-size: 14pt;
+                vertical-align: middle;
+                padding-bottom: 3px;
             }
             `}</style>
             </div>
@@ -41,9 +60,9 @@ class UserAuth extends Component {
             <div>
             <form onSubmit={this.handle_login_form} >
             <input id="email" type="text" ref={this.email}
-                placeholder="epost" maxLength="30" />
+            placeholder="epost" maxLength="30" />
             <input id="password" type="password" ref={this.password} 
-                placeholder="passord" maxLength="4096" />
+            placeholder="passord" maxLength="4096" />
             <input type="submit" value="Logg inn" />
             </form> eller <a href="/register/">registrer ny bruker</a>
             <style jsx>{`
@@ -64,6 +83,9 @@ class UserAuth extends Component {
                     width: 160px;
                     padding: 1px 4px;
                 }
+                a, a:link{
+                    color: inherit;
+                }
             `}</style>
             </div>
         )
@@ -83,6 +105,8 @@ class UserAuth extends Component {
             .userBar {
                 padding: 0 40px;
                 background: black;
+                color: white;
+                font-family: 'Roboto', sans-serif;
             }
             `}</style>
             </div>
@@ -96,10 +120,43 @@ class UserAuth extends Component {
             user_id: null
         });
     }
-    
+
     handle_login_form = (e, data) => {
         e.preventDefault();
         this.get_token(this.email.current.value, this.password.current.value);
+    };
+
+    NotLoggedInException() {};
+
+    load_profile_data = () => {
+        fetch(env.API_BASE_URL + 'user',  {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ' + this.state.token,
+            }
+        })
+            .then(res => {
+                if (res.status == 401) {
+                    throw new this.NotLoggedInException();
+                } else {
+                    return res.json();
+                }
+            })
+            .then(json => {
+                this.setState({
+                    email: json.email,
+                    first_name: json.first_name,
+                    last_name: json.last_name,
+                })
+            })
+            .catch(e => {
+                if (e instanceof this.NotLoggedInException) {
+                    this.handle_logout();
+                } else {
+                    throw(e);
+                }
+            })
     }
 
     get_token = (email, password) => {
@@ -110,14 +167,21 @@ class UserAuth extends Component {
                 'Authorization': 'Basic ' + btoa(email + ":" + password),
             },
         })
-        .then(res => res.json())
-        .then(json => {
-            cookies.set('token', json.key);
-            this.setState({
-                token: json.key,
-                user_id: json.user
-            });
-        });
+            .then(res => {
+                if (res.status == 401) {
+                    throw new this.NotLoggedInException();
+                } else {
+                    return res.json();
+                }
+            })
+            .then(json => {
+                cookies.set('token', json.key);
+                this.setState({
+                    token: json.key,
+                });
+                this.load_profile_data();
+            })
+            .catch(e => console.log(e));
     }
 }
 
