e0f7eae8405c90da1302de8f7860bc789e24cf62
Tore Sinding Bekkedal <toresbe@gmail.com>
2020-02-08 15:32:13 +0100

    experimenting with live stream

--- packages/frontend/components/LiveNow.js
+++ packages/frontend/components/LiveNow.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-unfetch'
 import {LiveVideoPlayer, ScheduleInfo} from './LiveVideoPlayer.js'
+import DASHPlayer from '../components/DASHPlayer.js';
+//import VideoPlayer from '../components/VideoJS.js';
+//let ShakaPlayer;
+//if(typeof window !== 'undefined') {
+//    ShakaPlayer = require('shaka-player-react');
+//} else {
+//    ShakaPlayer = () => {return <></>};
+//}
 
 class LiveNow extends Component {
+
     render = () => {
+        const videoJsOptions = {
+          autoplay: true,
+          controls: true,
+            fluid: true,
+          sources: [{
+              src: '//beta.frikanalen.no/stream/frikanalen/frikanalen.mpd',
+              type: 'application/dash+xml',
+          }]
+        }
+
+            //<VideoPlayer { ...videoJsOptions } />
         return (
         <div id="live_now">
             <div className="header">direkte nå</div>
-            <LiveVideoPlayer />
+            <DASHPlayer manifestUri='https://beta.frikanalen.no/stream/dash/pgm/pgm_hi/index.mpd' />
             <ScheduleInfo />
             <style jsx>{`
                 #live_now {
@@ -29,6 +49,11 @@ class LiveNow extends Component {
                     }
                 }
 
+            `}</style>
+            <style jsx global>{`
+                #live_now>div {
+                    padding:0;
+                }
             `}</style>
         </div>
         );
---
+++ packages/frontend/components/VideoJS.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import videojs from 'video.js'
+
+export default class VideoPlayer extends React.Component {
+  componentDidMount() {
+    videojs.log.level('all')
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+ //   this.log.level('all');
+//      console.log('onPlayerReady', this);
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div>	
+        <div data-vjs-player>
+          <video ref={ node => this.videoNode = node } className="video-js"></video>
+        </div>
+      </div>
+    )
+  }
+}
