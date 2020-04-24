bde38ac96af633dfa62b356fa66c7cf3632851a9
Tore Sinding Bekkedal <toresbe@gmail.com>
2020-04-24 23:24:37 +0200

    move formålsparagraf to the top

--- packages/frontend/pages/om.js
+++ packages/frontend/pages/om.js
@@ -44,7 +44,8 @@ export var about_render = (data) => {
                 margin-left: 20px;
                 align-self:stretch;
                 padding-left: 15px;
-                padding-right: 40px;
+                padding-right: 15px;
+                font-size: 20px;
             }
 
             #leftbar {
@@ -74,20 +75,47 @@ export var about_render = (data) => {
 export default function About() {
   return about_render(
       <div>
-      <h2>Frikanalen er sivilsamfunnets videoplatform.</h2>
-      <p>Målet med Frikanalen er å styrke ytringsfriheten og det deltakende
+      <div id="vedtektsitat">
+      <p id="quote">Målet med Frikanalen er å styrke ytringsfriheten og det deltakende
       demokratiet gjennom å gi flere mulighet til å ytre seg gjennom TV-mediet.</p>
-      <p>Vi ønsker i samarbeide med våre medlemsorganisasjoner utvikle en videoplattform
-      tilrettelagt for sivilsamfunnets behov, uten forhåndssensur.</p>
+      <p id="cite">Frikanalens formålsparagraf</p>
+      </div>
+      <div id="blurb">
+      <h2>Frikanalen er sivilsamfunnets videoplatform.</h2>
+      <p>Vi ønsker i samarbeide med våre medlemsorganisasjoner utvikle en
+      videoplattform tilrettelagt for behovene til norsk organisasjonsliv og
+      frivillighet, uten forhåndssensur.</p>
       <p>Vi tilbyr alle våre medlemmer adgang til en riksdekkende TV-kanal med
       formidlingsplikt - som vil si at alle kabelleverandører er på samme måte
-      som NRK, forpliktet til å bære vår kanal. Vi er allerede båret på RiksTV,
+      som NRK, forpliktet til å bære vår kanal. Vi er allerede båret av RiksTV,
       og er i forhandlinger med Telia og Telenor om å få vår kanal båret av
       dem.</p>
       <p>Det pågår også et arbeide med å tilrettelegge for brukerstyrte
       direktesendinger rett ut til vår TV-kanal. I tillegg til dette er vi i
       full gang med å utvikle en god løsning for spredning av video på
       nett. Så følg med!</p>
+      </div>
+      <style jsx>{`
+            #vedtektsitat {
+                background: white;
+                opacity: 0.8;
+                color: black;
+                padding: 3px 20px;
+                font-size: 20px;
+                margin-top: 15px;
+                min-width: 100%;
+            }
+
+            #quote {
+               font-style: italic; 
+            }
+
+            #cite {
+               text-align: right;
+               margin-right: 30px;
+               opacity: 0.8;
+               }
+      `}</style>
       </div>
   );
 }
