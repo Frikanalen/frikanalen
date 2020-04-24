4b0c5373cac8c5812520461f4c2b69d9f094d8e1
Tore Sinding Bekkedal <toresbe@gmail.com>
2020-04-24 23:24:21 +0200

    I think the background is a bit too dark

--- packages/frontend/components/Layout.js
+++ packages/frontend/components/Layout.js
@@ -63,7 +63,7 @@ const Layout = props => (
             grid-template-area: footer;
         }
         html, body {
-            background: #a6a6a6;
+            background: #b6b6b6;
             width: 100%;
             height: 100%;
             margin: 0;
