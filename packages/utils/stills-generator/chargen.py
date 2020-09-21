207d4efbe36a213147a9503b17cc12db5eebbfdc
Tore Sinding Bekkedal <toresbe@protonmail.com>
2020-09-21 15:58:50 +0200

    implement raw RGBA output

--- packages/utils/stills-generator/chargen.py
+++ packages/utils/stills-generator/chargen.py
@@ -4,19 +4,30 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import io
 
-class CharacterGenerator():
-    def __init__(self):
+class Poster():
+    def __init__(self, fields):
         self.heading_font = ImageFont.truetype('./Roboto-Black.ttf', 72)
         self.body_font = ImageFont.truetype('./Roboto-Black.ttf', 52)
+        self.heading = fields['heading']
+        self.text = fields['text']
+        self.poster = Image.open('background.png')
+        self.width, self.height = (1280, 720)
 
-    def render(self, heading, text):
-        width, height = (1280, 720)
-        poster = Image.open('background.png')
-        draw = ImageDraw.Draw(poster)
+    def render(self):
+        draw = ImageDraw.Draw(self.poster)
 
-        draw.multiline_text((142, 180), heading, font=self.heading_font, fill='#d66969')
-        draw.multiline_text((195, 300), text, font=self.body_font, fill='#78bddb')
+        draw.multiline_text((142, 180), self.heading, font=self.heading_font, fill='#d66969')
+        draw.multiline_text((195, 300), self.text, font=self.body_font, fill='#78bddb')
 
+    def _export(self, format):
+        self.render()
         buf = io.BytesIO()
-        poster.save(buf, 'PNG')
+        self.poster.save(buf, format)
         return buf.getvalue()
+
+    def getPNG(self):
+        return self._export('PNG')
+
+    def getRGBA(self):
+        self.render()
+        return self.poster.tobytes()
