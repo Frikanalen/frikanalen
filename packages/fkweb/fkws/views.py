669978a3ce328c17432648bad6ca9a5c86994b94
Tore Sinding Bekkedal <toresbe@protonmail.com>
2020-09-22 14:49:10 +0200

    implement some experimental caching of schedule view

--- packages/fkweb/fkws/views.py
+++ packages/fkweb/fkws/views.py
@@ -3,13 +3,19 @@
 
 import csv
 import datetime
+import pytz
 
+from django.core.cache import cache
+from django.views.decorators.cache import cache_control
+from django.views.decorators.cache import never_cache
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 from django_filters import rest_framework as djfilters
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
@@ -168,22 +174,58 @@ class ScheduleitemList(generics.ListCreateAPIView):
     pagination_class = Pagination
     permission_classes = (IsInOrganizationOrReadOnly,)
 
+    def parse_YYYYMMDD_or_today(self, inputDate):
+        try:
+            return datetime.datetime.strptime(inputDate, '%Y%m%d')\
+                .astimezone(pytz.timezone('Europe/Oslo'))
+        except (KeyError, ValueError, TypeError):
+            return datetime.datetime.now().astimezone(pytz.timezone('Europe/Oslo'))
+
+    def parse_int_or_7(self, inputDays):
+        try:
+            return int(inputDays)
+        except (KeyError, ValueError, TypeError):
+            return 7
+
     def get_queryset(self):
         params = self.request.query_params
-        try:
-            days = int(params['days'])
-        except (KeyError, ValueError):
-            days = 7
-        try:
-            date = datetime.datetime.strptime(params['date'], '%Y%m%d')
-        except (KeyError, ValueError):
-            date = datetime.date.today()
+        date = self.parse_YYYYMMDD_or_today(params.get('date', None))
+        days = self.parse_int_or_7(params.get('days', None))
+
         # FIXME by_day should be on queryset but need to upgrade
         # django first
         queryset = Scheduleitem.objects.by_day(
             date=date, days=days, surrounding=bool(params.get('surrounding')))
+
         return queryset.order_by('starttime')
 
+    def dispatch(self, request, *args, **kwargs):
+        print([x for x in request.GET.items()])
+        params = request.GET
+        res = super().dispatch(request, *args, **kwargs)
+
+        cacheable = True
+        if params.get('surrounding') != None: cacheable = False
+        if params.get('ordering') != None: cacheable = False
+        if params.get('page_size') != None: cacheable = False
+
+        if cacheable:
+            date = self.parse_YYYYMMDD_or_today(params.get('date', None))
+            days = self.parse_int_or_7(params.get('days', None))
+            renderer = type(res.accepted_renderer).__name__
+
+            cache_key = 'schedule-%s-%s-%s' % (date.strftime('%Y%m%d'), days, renderer)
+            cache_res = cache.get(cache_key)
+
+            if cache_res:
+                return cache_res
+
+        res.render()
+
+        if res.status_code == 200:
+            cache.set(cache_key, res, 50)
+
+        return res
 
 class ScheduleitemDetail(generics.RetrieveUpdateDestroyAPIView):
     """
