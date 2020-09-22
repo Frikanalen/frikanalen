9a13c09e0a0abbaddb1970b847d276608d663b12
Tore Sinding Bekkedal <toresbe@protonmail.com>
2020-09-22 02:30:20 +0200

    revert regression

--- packages/frontend/components/API/Organization.js
+++ packages/frontend/components/API/Organization.js
@@ -1,11 +1,6 @@
 import { APIGET, APIPOST } from "./Fetch.js";
 
 export default class Organization {
-  constructor(id) {
-    this.ID = id;
-    this.load(id);
-  }
-
   loadJSON(orgData) {
     this.ID = orgData.id;
     this.name = orgData.name;
