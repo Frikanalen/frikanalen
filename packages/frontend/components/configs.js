dd637661fd99a8b8099c9ba749468f21ef1382bc
Tore Sinding Bekkedal <toresbe@protonmail.com>
2020-09-25 12:15:21 +0200

    remove graphql URL

--- packages/frontend/components/configs.js
+++ packages/frontend/components/configs.js
@@ -3,17 +3,14 @@ const env = process.env.NEXT_PUBLIC_ENV || "development";
 const configs = {
   development: {
     api: "http://localhost:8080/api/",
-    graphql: "https://frikanalen.no/graphql",
     atem: "https://frikanalen.no/playout/atem/program",
   },
   staging: {
     api: "https://frikanalen.no/api/",
-    graphql: "https://frikanalen.no/graphql",
     atem: "https://frikanalen.no/playout/atem/program",
   },
   production: {
     api: "https://frikanalen.no/api/",
-    graphql: "https://frikanalen.no/graphql",
     atem: "https://frikanalen.no/playout/atem/program",
   },
 }[env];
