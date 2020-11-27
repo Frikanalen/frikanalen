1bb76882387b4878e14d0a3166e5fdcd68508a64
Tore Sinding Bekkedal <toresbe@protonmail.com>
2020-11-27 17:04:59 +0100

    Remove legacy on-air graphics (migrated to React)

--- packages/utils/on-air-graphics/clock/background.js
+++
@@ -1,7 +0,0 @@
-const pattern = trianglify({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    xColors: 'YlGn',
-    cellSize: 60
-  })
-  document.body.style.background = 'url(' + pattern.toCanvas().toDataURL() + ')'
