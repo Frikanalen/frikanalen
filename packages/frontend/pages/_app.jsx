b9f19436c2557606437b89bd7aa2ca05880e4aca
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-03-31 19:38:15 +0200

    experiment with Sentry use

--- packages/frontend/pages/_app.jsx
+++ packages/frontend/pages/_app.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from "react";
 import Router from "next/router";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shaka-player/dist/controls.css";
 import "shaka-player/dist/demo.css";
@@ -10,6 +12,16 @@ import PropTypes from "prop-types";
 
 import { getUserProfile } from "components/TS-API/API";
 import { UserContext } from "../components/UserContext";
+
+Sentry.init({
+  dsn: "https://41ab0b4801094dfd8ecd84eafc947380@o310671.ingest.sentry.io/5701229",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 function MyApp({ Component, pageProps }) {
   const [token, setToken] = useState(null);
