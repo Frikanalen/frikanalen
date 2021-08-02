c8a77f20ad351e47c34006e936feb91c346a0b26
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-08-02 16:21:02 +0200

    Refactoring continues

--- infra/k8s-dev/deploy.sh
+++ infra/k8s-dev/deploy.sh
@@ -2,6 +2,14 @@
 k="microk8s kubectl"
 export BASEPATH="../k8s"
 
+echo "Enabling required addons"
+
+microk8s enable dns storage traefik linkerd metallb
+
+echo "Customizing traefik to play nice with metallb"
+
+$k apply -f traefik.yaml
+
 echo "Creating backend namespace"
 
 $k apply -f $BASEPATH/database/000-namespace.yaml
@@ -11,11 +19,11 @@ echo "Creating database/backend secrets"
 SECRET_KEY=$(base64 /dev/urandom | head -c50)
 DATABASE_PASS=$(pwgen 32 1)
 
-$k apply secret -n database generic backend-secrets \
+$k create secret -n database generic backend-secrets \
     --from-literal=SECRET_KEY=${SECRET_KEY} \
     --from-literal=DATABASE_PASS=${DATABASE_PASS} \
 
-$k apply secret -n default generic backend-secrets \
+$k create secret -n default generic backend-secrets \
     --from-literal=SECRET_KEY=${SECRET_KEY} \
     --from-literal=DATABASE_PASS=${DATABASE_PASS} \
 
@@ -36,10 +44,7 @@ $k apply -f $BASEPATH/django/002-service.yaml
 
 echo "Opening ingress..."
 
-$k apply -f ingress-service.yaml
-$k apply -f ingress-backend.yaml
-
-
+$k apply -f ingress.yaml
 
 #$k apply -f $BASEPATH/django/periodic-schedule-tasks/001-fill_agenda_with_jukebox.yaml
 #$k apply -f $BASEPATH/django/periodic-schedule-tasks/001-fill_agenda.yaml
