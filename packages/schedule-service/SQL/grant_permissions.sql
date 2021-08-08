f87190e84d9dae08d56465b20a1baa2e371bb8cf
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-08-08 16:56:02 +0200

    Remove vestigial SQL files

--- packages/schedule-service/SQL/grant_permissions.sql
+++
@@ -1,3 +0,0 @@
-GRANT SELECT ON fk_video, fk_scheduleitem, fk_organization TO fkschedule;
-GRANT INSERT ON fk_scheduleitem to fkschedule;
-GRANT USAGE, SELECT ON SEQUENCE "ScheduleItem_id_seq" to fkschedule;
--- packages/schedule-service/sql_init.sql
+++
@@ -1,3 +0,0 @@
-/* CREATE ROLE fkschedule LOGIN PASSWORD 'temporary';*/
-
-grant select on fk_scheduleitem, fk_schedulepurpose, fk_video, fk_organization to fkschedule;
