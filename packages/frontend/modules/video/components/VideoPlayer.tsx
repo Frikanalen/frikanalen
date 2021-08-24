f9384af65c9c151b7b102f4bf5b259fb89e55481
Enitoni <enitoni@gmail.com>
2021-08-24 21:16:15 +0200

    add VideoPlayer wrapper

---
+++ packages/frontend/modules/video/components/VideoPlayer.tsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+import { AspectContainer } from "modules/core/components/AspectContainer";
+import dynamic from "next/dynamic";
+
+// No types for this library
+// @ts-ignore
+const ShakaPlayer = dynamic(() => import("shaka-player-react"), { ssr: false }) as any;
+
+const Container = styled.div`
+  position: relative;
+
+  > div {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+  }
+
+  border-radius: 4px;
+  overflow: hidden;
+
+  height: 100%;
+  width: 100%;
+
+  box-shadow: 2px 2px 11px 2px rgba(0, 0, 0, 0.1);
+`;
+
+export type VideoPlayerProp = {
+  src: string;
+  width: number;
+  height: number;
+};
+
+export function VideoPlayer(props: VideoPlayerProp) {
+  const { src, width, height } = props;
+
+  return (
+    <AspectContainer width={width} height={height}>
+      <Container>
+        <ShakaPlayer src={src} />
+      </Container>
+    </AspectContainer>
+  );
+}
