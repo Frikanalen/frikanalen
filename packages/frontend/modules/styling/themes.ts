715756d17cc700e4e6e745624f53a11c5b83ae12
Enitoni <enitoni@gmail.com>
2021-08-24 02:39:30 +0200

    add lightTheme

---
+++ packages/frontend/modules/styling/themes.ts
@@ -0,0 +1,14 @@
+import { Theme } from "@emotion/react";
+
+export const lightTheme: Theme = {
+  color: {
+    card: "rgba(255, 255, 255, 0.6)",
+    accent: "#E88840",
+    secondAccent: "#F15645",
+    thirdAccent: "#64BC50",
+  },
+  fontColor: {
+    normal: "rgba(0, 0, 0, 0.85)",
+    muted: "rgba(0, 0, 0, 0.7)",
+  },
+};
