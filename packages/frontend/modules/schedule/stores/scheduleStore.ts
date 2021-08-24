94d132319f91742b27032d60bab49b4ec1b69235
Enitoni <enitoni@gmail.com>
2021-08-25 00:25:55 +0200

    make scheduleStore serializable

--- packages/frontend/modules/schedule/stores/scheduleStore.ts
+++ packages/frontend/modules/schedule/stores/scheduleStore.ts
@@ -4,10 +4,16 @@ import { ApiCollection } from "modules/network/types";
 import { createStoreFactory, Store } from "modules/state/classes/Store";
 import { ScheduleItem } from "../types";
 
-export class ScheduleStore extends Store {
+export type SerializedScheduleStore = {
+  items: ScheduleItem[];
+};
+
+export class ScheduleStore extends Store<SerializedScheduleStore> {
   @observable public items: ScheduleItem[] = [];
 
   public async fetch() {
+    if (this.items.length > 0) return;
+
     const response = await api.get<ApiCollection<ScheduleItem>>("/scheduleitems", {
       params: {
         days: 1,
@@ -15,6 +21,16 @@ export class ScheduleStore extends Store {
     });
 
     this.items = response.data.results;
+  }
+
+  public serialize() {
+    return {
+      items: this.items,
+    };
+  }
+
+  public hydrate(data: SerializedScheduleStore) {
+    this.items = data.items;
   }
 }
 
