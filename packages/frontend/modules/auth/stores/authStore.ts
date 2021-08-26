0f0425d2e5998fd4449969768182ce802dfad151
Enitoni <enitoni@gmail.com>
2021-08-26 18:55:34 +0200

    Add authStore

---
+++ packages/frontend/modules/auth/stores/authStore.ts
@@ -0,0 +1,42 @@
+import { AxiosError } from "axios";
+import { computed, observable } from "mobx";
+import { api } from "modules/network";
+import { createStoreFactory, Store } from "modules/state/classes/Store";
+import { StoredValue } from "modules/state/classes/StoredValue";
+import { User } from "modules/user/schemas";
+
+// TODO: FIX THIS AS SOON AS POSSIBlE. LOCAL STORAGE IS NOT SAFE FOR AUTH TOKENS.
+const unsafeLocalStorageHack = new StoredValue<string | undefined>("token", undefined);
+
+export class AuthStore extends Store {
+  @observable user?: User;
+
+  public async authenticate() {
+    const token = unsafeLocalStorageHack.restore();
+    if (!token) return;
+
+    try {
+      const response = await api.get<User>("/user", {
+        headers: { Authorization: `Token ${token}` },
+      });
+
+      this.user = response.data;
+    } catch (error) {
+      const { response } = error as AxiosError;
+
+      if (response?.status === 401) {
+        // TODO: Handle invalid token
+        return;
+      }
+
+      throw error;
+    }
+  }
+
+  @computed
+  public get isAuthenticated() {
+    return !!this.user;
+  }
+}
+
+export const authStore = createStoreFactory(AuthStore);
--- packages/frontend/modules/state/stores.ts
+++ packages/frontend/modules/state/stores.ts
@@ -1,10 +1,13 @@
+import { authStore, AuthStore } from "modules/auth/stores/authStore";
 import { ScheduleStore, scheduleStore } from "modules/schedule/stores/scheduleStore";
 import { StoreFactories } from "./types";
 
 export type Stores = {
+  authStore: AuthStore;
   scheduleStore: ScheduleStore;
 };
 
 export const stores: StoreFactories = {
+  authStore,
   scheduleStore,
 };
