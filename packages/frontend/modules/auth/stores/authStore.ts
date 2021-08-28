4b8a0b33caa75a637615a42754e4cda2d9dcc6b0
Enitoni <enitoni@gmail.com>
2021-08-29 00:04:59 +0200

    Remove dead code from authStore

--- packages/frontend/modules/auth/stores/authStore.ts
+++ packages/frontend/modules/auth/stores/authStore.ts
@@ -1,12 +1,7 @@
-import { AxiosError } from "axios";
 import { computed, observable } from "mobx";
 import { api } from "modules/network";
 import { createStoreFactory, Store } from "modules/state/classes/Store";
-import { StoredValue } from "modules/state/classes/StoredValue";
 import { User } from "modules/user/schemas";
-
-// TODO: FIX THIS AS SOON AS POSSIBlE. LOCAL STORAGE IS NOT SAFE FOR AUTH TOKENS.
-const unsafeLocalStorageHack = new StoredValue<string | undefined>("token", undefined);
 
 export class AuthStore extends Store {
   @observable user?: User;
@@ -17,13 +12,6 @@ export class AuthStore extends Store {
 
       this.user = response.data;
     } catch (error) {
-      const { response } = error as AxiosError;
-
-      if (response?.status === 401) {
-        // TODO: Handle invalid token
-        return;
-      }
-
       throw error;
     }
   }
