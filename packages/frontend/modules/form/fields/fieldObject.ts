443815b84ee63b55861e367f7000e4137018d454
Enitoni <enitoni@gmail.com>
2021-08-28 23:27:03 +0200

    the other error that got away

--- packages/frontend/modules/form/fields/fieldObject.ts
+++ packages/frontend/modules/form/fields/fieldObject.ts
@@ -1,87 +1,85 @@
-import { FieldsType } from "../classes/ObservableForm"
-import { Manager } from "../../state/types/Manager"
-import { computed, observable } from "mobx"
-import { checkIfFieldIsReady } from "../helpers/checkIfFieldIsReady"
-import { ValidatorList } from "../classes/ValidatorList"
+import { FieldsType } from "../classes/ObservableForm";
+import { computed, observable } from "mobx";
+import { checkIfFieldIsReady } from "../helpers/checkIfFieldIsReady";
+import { ValidatorList } from "../classes/ValidatorList";
+import { Manager } from "modules/state/types";
 
 export class ObservableFieldObject<F extends FieldsType> {
-  protected manager!: Manager
-  protected validators: ValidatorList<F>
+  protected manager!: Manager;
+  protected validators: ValidatorList<F>;
 
-  @observable public touched = false
+  @observable public touched = false;
 
   public constructor(public fields: F) {
-    this.validators = new ValidatorList(this.fields)
+    this.validators = new ValidatorList(this.fields);
 
     // Validate all fields within
     this.validators.add(async (value) => {
-      let invalid = false
+      let invalid = false;
 
       for (const field of Object.values(value)) {
-        await field.validate()
+        await field.validate();
 
         if (field.error) {
-          invalid = true
-          break
+          invalid = true;
+          break;
         }
       }
 
-      return invalid ? "One or more fields are invalid" : ""
-    })
+      return invalid ? "One or more fields are invalid" : "";
+    });
   }
 
   /** Must be called first before doing anything else */
   public setManager(manager: Manager) {
-    this.manager = manager
-    Object.values(this.fields).forEach((x) => x.setManager(manager))
+    this.manager = manager;
+    Object.values(this.fields).forEach((x) => x.setManager(manager));
   }
 
   @computed
   public get serializedValue(): any {
-    return Object.fromEntries(
-      Object.entries(this.fields).map(([k, f]) => [k, f.serializedValue]),
-    )
+    return Object.fromEntries(Object.entries(this.fields).map(([k, f]) => [k, f.serializedValue]));
   }
 
   @computed
   public get ready() {
     for (const field of Object.values(this.fields)) {
-      const ready = checkIfFieldIsReady(field)
+      const ready = checkIfFieldIsReady(field);
 
       if (!ready) {
-        return false
+        return false;
       }
     }
 
-    return true
+    return true;
   }
 
   public touch = () => {
-    this.touched = true
+    this.touched = true;
 
     for (const field of Object.values(this.fields)) {
-      field.touch()
+      field.touch();
     }
 
-    this.validators.validate(this.fields)
-  }
+    this.validators.validate(this.fields);
+  };
 
   public validate = async () => {
-    await this.validators.validate(this.fields)
-  }
+    await this.validators.validate(this.fields);
+  };
 
   public destroy() {
     for (const field of Object.values(this.fields)) {
-      field.destroy()
+      field.destroy();
     }
   }
 
   @computed
   public get error() {
-    return this.validators.error
+    return this.validators.error;
   }
 }
 
 export const fieldObject = <F extends FieldsType>(fields: F) => {
-  return new ObservableFieldObject(fields)
-}
+  return new ObservableFieldObject(fields);
+};
