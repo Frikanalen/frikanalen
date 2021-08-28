65854011bb9dd9650eff177be2b62c6c995f0482
Enitoni <enitoni@gmail.com>
2021-08-29 00:05:27 +0200

    add link to switch to RegisterModal in LoginModal

--- packages/frontend/modules/auth/components/LoginModal.tsx
+++ packages/frontend/modules/auth/components/LoginModal.tsx
@@ -5,9 +5,10 @@ import { ControlledTextInput } from "modules/input/components/ControlledTextInpu
 import { PrimaryModal } from "modules/modal/components/PrimaryModal";
 import { useModal } from "modules/modal/hooks/useModal";
 import { api } from "modules/network";
-import { useStores } from "modules/state/manager";
+import { useManager } from "modules/state/manager";
 import { GenericButton } from "modules/ui/components/GenericButton";
 import { LoginForm } from "../forms/createLoginForm";
+import { spawnRegisterModal } from "../helpers/spawnRegisterModal";
 
 export type LoginModalProps = {
   form: LoginForm;
@@ -17,10 +18,17 @@ const Field = styled(FormField)`
   margin-bottom: 16px;
 `;
 
+const RegisterLink = styled.a`
+  cursor: pointer;
+`;
+
 export function LoginModal(props: LoginModalProps) {
-  const { authStore } = useStores();
-  const modal = useModal();
   const { form } = props;
+
+  const manager = useManager();
+  const modal = useModal();
+
+  const { authStore } = manager.stores;
 
   const handleSubmit = async () => {
     const valid = await form.ensureValidity();
@@ -37,17 +45,23 @@ export function LoginModal(props: LoginModalProps) {
     }
   };
 
+  const handleRegisterClick = () => {
+    modal.dismiss();
+    spawnRegisterModal(manager);
+  };
+
   return (
     <PrimaryModal.Container>
       <Form onSubmit={handleSubmit} form={form}>
         <PrimaryModal.Header title="Logg inn" />
         <PrimaryModal.Body>
           <Field label="E-post" name="email">
-            <ControlledTextInput placeholder="ola-nordmann@addresse.no" autoFocus name="email" />
+            <ControlledTextInput placeholder="epost@webside.no" autoFocus name="email" />
           </Field>
           <Field label="Passord" name="password">
             <ControlledTextInput type="password" name="password" />
           </Field>
+          <RegisterLink onClick={handleRegisterClick}>Registrer ny konto?</RegisterLink>
         </PrimaryModal.Body>
         <PrimaryModal.Footer>
           <PrimaryModal.Actions>
