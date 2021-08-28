2c6a89ec0f0b6b4148c6789657b255545486fece
Enitoni <enitoni@gmail.com>
2021-08-28 21:43:04 +0200

    add spawnLoginModal

---
+++ packages/frontend/modules/auth/helpers/spawnLoginModal.ts
@@ -0,0 +1,15 @@
+import { Manager } from "modules/state/types";
+import React from "react";
+import { LoginModal } from "../components/LoginModal";
+import { createLoginForm } from "../forms/createLoginForm";
+
+export const spawnLoginModal = (manager: Manager) => {
+  const { modalStore } = manager.stores;
+  const form = createLoginForm(manager);
+
+  modalStore.spawn({
+    key: "login",
+    render: () => React.createElement(LoginModal, { form }),
+    closeOnClickout: true,
+  });
+};
