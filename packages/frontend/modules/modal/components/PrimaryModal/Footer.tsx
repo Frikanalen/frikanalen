2804ddb39a311b9b655cd5ffaa0fccc8ccedb95c
Enitoni <enitoni@gmail.com>
2021-08-29 15:11:06 +0200

    add some default flex styling to Footer.tsx

--- packages/frontend/modules/modal/components/PrimaryModal/Footer.tsx
+++ packages/frontend/modules/modal/components/PrimaryModal/Footer.tsx
@@ -3,4 +3,8 @@ import styled from "@emotion/styled";
 export const Footer = styled.div`
   padding: 24px;
   padding-top: 0px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
