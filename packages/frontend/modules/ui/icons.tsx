b0c310fcc2da80027a62aff55d6564c09e488afb
Enitoni <enitoni@gmail.com>
2021-08-30 01:55:42 +0200

    add chevronLeft and chevronRight icons

--- packages/frontend/modules/ui/icons.tsx
+++ packages/frontend/modules/ui/icons.tsx
@@ -4,6 +4,18 @@ export const chevronDown = (
   </svg>
 );
 
+export const chevronLeft = (
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+  </svg>
+);
+
+export const chevronRight = (
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+  </svg>
+);
+
 export const user = (
   <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path
