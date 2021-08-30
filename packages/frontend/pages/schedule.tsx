02225c3814a806d56d4fe330077786b9e145713a
Enitoni <enitoni@gmail.com>
2021-08-30 20:01:19 +0200

    add schedule item viewing on schedule page

--- packages/frontend/pages/schedule.tsx
+++ packages/frontend/pages/schedule.tsx
@@ -1,43 +1,60 @@
 import styled from "@emotion/styled";
 import { useObserver } from "mobx-react-lite";
 import { CalendarInput } from "modules/input/components/CalendarInput";
+import { ScheduleTimelineItem } from "modules/schedule/components/ScheduleTimelineItem";
 import { humanizeSelectedScheduleDate } from "modules/schedule/helpers/humanizeSelectedScheduleDate";
 import { useStores } from "modules/state/manager";
+import { NextPageContext } from "next";
 
 const Container = styled.div`
   display: flex;
 `;
 
 const Sidebar = styled.div`
-  width: 350px;
+  width: 320px;
 
-  border-right: solid 2px ${(props) => props.theme.color.divider};
-  padding-right: 24px;
-  margin-right: 24px;
+  border-left: solid 2px ${(props) => props.theme.color.divider};
+  padding-left: 32px;
+  margin-left: 32px;
 `;
 
 const Result = styled.div`
   flex: 1;
 `;
 
-const DayTitle = styled.h2``;
+const DayTitle = styled.h2`
+  margin-bottom: 16px;
+`;
 
 export default function Schedule() {
   const { scheduleStore } = useStores();
 
   const selectedDate = useObserver(() => scheduleStore.selectedDate);
+  const selectedDateItems = useObserver(() => scheduleStore.selectedDateItems);
+
   const handleSelect = (date: Date) => {
     scheduleStore.selectedDate = date;
+    scheduleStore.fetchByDate(date);
   };
 
   return (
     <Container>
+      <Result>
+        <DayTitle>{humanizeSelectedScheduleDate(selectedDate)}</DayTitle>
+        {selectedDateItems.map((i) => (
+          <ScheduleTimelineItem key={i.id} item={i} />
+        ))}
+      </Result>
       <Sidebar>
         <CalendarInput value={selectedDate} onChange={handleSelect} />
       </Sidebar>
-      <Result>
-        <DayTitle>{humanizeSelectedScheduleDate(selectedDate)}</DayTitle>
-      </Result>
     </Container>
   );
 }
+
+Schedule.getInitialProps = async (context: NextPageContext) => {
+  const { scheduleStore } = context.manager.stores;
+  await scheduleStore.fetchByDate(scheduleStore.selectedDate);
+
+  return {};
+};
