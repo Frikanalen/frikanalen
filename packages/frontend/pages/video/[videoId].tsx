6eabfc38a80066384d4cdf80c67a6e958a79c0d3
Sebastian Olsen <enitoni@gmail.com>
2021-09-21 17:43:52 +0200

    add missing info to video page

--- packages/frontend/pages/video/[videoId].tsx
+++ packages/frontend/pages/video/[videoId].tsx
@@ -1,21 +1,74 @@
 import styled from "@emotion/styled";
+import { format } from "date-fns";
+import { nb } from "date-fns/locale";
 import { createResourcePageWrapper } from "modules/state/helpers/createResourcePageWrapper";
 import { Video } from "modules/video/classes/Video";
 import { VideoPlayer } from "modules/video/components/VideoPlayer";
+import Link from "next/link";
+import React from "react";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
+
+const Content = styled.div`
+  flex: 1;
+`;
+
+const PrimaryInfo = styled.div`
+  margin-top: 16px;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  margin-bottom: 2px;
+`;
+
+const Organization = styled.h3`
+  font-size: 1.1em;
+  font-weight: 400;
+
+  margin-bottom: 12px;
+`;
+
+const Description = styled.p`
+  white-space: pre-wrap;
+  word-break: break-word;
+`;
+
+const UploadedDate = styled.span`
+  color: ${(props) => props.theme.fontColor.muted};
+`;
+
+const Sidebar = styled.div`
+  width: 320px;
+  margin-left: 32px;
+`;
 
 export type ContentProps = {
   video: Video;
 };
 
-function Content(props: ContentProps) {
+function VideoView(props: ContentProps) {
   const { video } = props;
-  const { ogvUrl, files } = video.data;
+  const { name, createdTime, header, organization, ogvUrl, files } = video.data;
 
   return (
     <Container>
-      <VideoPlayer width={1280} height={720} src={ogvUrl} thumbnail={files.largeThumb} />
+      <Content>
+        <VideoPlayer width={1280} height={720} src={ogvUrl} thumbnail={files.largeThumb} />
+        <PrimaryInfo>
+          <Title>{name}</Title>
+          <Organization>
+            <Link href={`/organization/${organization.id}`} passHref>
+              <a>{organization.name}</a>
+            </Link>
+          </Organization>
+        </PrimaryInfo>
+        <Description>{header}</Description>
+        <UploadedDate>lastet opp {format(new Date(createdTime), "d. MMM yyyy", { locale: nb })}</UploadedDate>
+      </Content>
+      <Sidebar></Sidebar>
     </Container>
   );
 }
@@ -28,7 +81,7 @@ const VideoPage = createResourcePageWrapper({
     const safeVideoId = Number(videoId) ?? 0;
     return videoStore.fetchById(safeVideoId);
   },
-  renderContent: (v) => <Content video={v} />,
+  renderContent: (v) => <VideoView video={v} />,
 });
 
 export default VideoPage;
