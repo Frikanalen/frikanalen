9fd71b5d040ab8b26f842b04996d75c047bad71f
Sebastian Olsen <enitoni@gmail.com>
2021-09-21 19:08:25 +0200

    add ListFactory type

--- packages/frontend/modules/state/types.ts
+++ packages/frontend/modules/state/types.ts
@@ -1,8 +1,11 @@
 import { Stores } from "./stores";
 import { StoreManager } from "./classes/StoreManager";
+import { List } from "./classes/List";
 
 export type Manager = StoreManager<Stores>;
 
 export type StoreFactories = {
   [K in keyof Stores]: (manager: Manager) => Stores[K];
 };
+
+export type ListFactory<D extends object, P extends object> = (data: D, manager: Manager) => List<any, P>;
