1262f42e5db2d89b2011ed622c307d628e79f108
Sebastian Olsen <enitoni@gmail.com>
2021-09-22 15:58:07 +0200

    Fix string validators not being in Norwegian

--- packages/frontend/modules/form/fields/string.ts
+++ packages/frontend/modules/form/fields/string.ts
@@ -1,57 +1,54 @@
-import {
-  ObservableFormField,
-  ObservableFormFieldOptions,
-} from "../classes/ObservableFormField"
+import { ObservableFormField, ObservableFormFieldOptions } from "../classes/ObservableFormField";
 
 const defaultOptions: ObservableFormFieldOptions<string> = {
   value: "",
-}
+};
 
 export class ObservableStringField extends ObservableFormField<string> {
-  public required(message = "This field is required") {
+  public required(message = "Dette feltet er påkrevd") {
     this.validators.add(async (value) => {
-      if (!value) return message
-      return ""
-    })
+      if (!value) return message;
+      return "";
+    });
 
-    return this
+    return this;
   }
 
-  public regex(regex: RegExp, message = "The regex does not match") {
+  public regex(regex: RegExp, message = "Samsvarer ikke med regex") {
     this.validators.add(async (value) => {
-      if (!value) return ""
+      if (!value) return "";
 
-      const match = value.match(regex)
+      const match = value.match(regex);
 
       if (match === null) {
-        return message
+        return message;
       }
 
-      return ""
-    })
+      return "";
+    });
 
-    return this
+    return this;
   }
 
-  public max(max: number, message = `Field cannot exceed ${max} characters`) {
+  public max(max: number, message = `Maks ${max} tegn kreves`) {
     this.validators.add(async (value) => {
-      if (!value) return ""
-      return value.length > max ? message : ""
-    })
+      if (!value) return "";
+      return value.length > max ? message : "";
+    });
 
-    return this
+    return this;
   }
 
-  public min(min: number, message = `Field must be at least ${min} characters`) {
+  public min(min: number, message = `Minimum ${min} tegn kreves`) {
     this.validators.add(async (value) => {
-      if (!value) return ""
-      return value.length < min ? message : ""
-    })
+      if (!value) return "";
+      return value.length < min ? message : "";
+    });
 
-    return this
+    return this;
   }
 }
 
 export const string = (options = defaultOptions) => {
-  return new ObservableStringField(options)
-}
+  return new ObservableStringField(options);
+};
