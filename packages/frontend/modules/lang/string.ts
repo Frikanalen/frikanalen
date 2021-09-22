3a4793b30f838f56e1a65e2ba5882ac639e1f890
Sebastian Olsen <enitoni@gmail.com>
2021-09-22 17:36:32 +0200

    add toTitleCase

--- packages/frontend/modules/lang/string.ts
+++ packages/frontend/modules/lang/string.ts
@@ -3,3 +3,17 @@ import { randomBytes } from "crypto";
 export const getHash = (bytes: number) => {
   return randomBytes(bytes).toString("hex");
 };
+
+export const toTitleCase = (str: string) => {
+  const splitStr = str.toLowerCase().split(" ");
+
+  const exceptions = ["i", "og", "for", "mot", "ved", "av"];
+
+  for (let i = 0; i < splitStr.length; i += 1) {
+    if (exceptions.indexOf(splitStr[i]) === -1) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1);
+    }
+  }
+
+  return splitStr.join(" ");
+};
