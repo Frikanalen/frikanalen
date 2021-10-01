be1c650c6349b895aef11d1b9fe5d7177198b2e9
Enitoni <enitoni@gmail.com>
2021-10-01 22:40:24 +0200

    add video list to Organization

--- packages/frontend/modules/organization/resources/Organization.ts
+++ packages/frontend/modules/organization/resources/Organization.ts
@@ -14,6 +14,18 @@ export type OrganizationData = {
   editorMsisdn: string;
 };
 
-export class Organization extends Resource<OrganizationData> {}
+export class Organization extends Resource<OrganizationData> {
+  public get videos() {
+    const { id } = this.data;
+    const { listStore } = this.manager.stores;
+
+    return listStore.ensure(`videos-organization-${id}`, "video", {
+      path: "/videos",
+      params: {
+        organization: id,
+      },
+    });
+  }
+}
 
 export const createOrganization: ResourceFactory<Organization> = (data, manager) => new Organization(data, manager);
