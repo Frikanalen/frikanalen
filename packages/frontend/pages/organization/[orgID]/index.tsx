42ebf4a1952437be66d9802643f5e29fcf465fb0
Enitoni <enitoni@gmail.com>
2021-10-02 00:14:22 +0200

    Refactor organization detail page

--- packages/frontend/pages/organization/[orgID]/index.tsx
+++ packages/frontend/pages/organization/[orgID]/index.tsx
@@ -1,91 +1,123 @@
+import styled from "@emotion/styled";
+import { Organization } from "modules/organization/resources/Organization";
+import { ListTail } from "modules/state/components/ListTail";
+import { createResourcePageWrapper } from "modules/state/helpers/createResourcePageWrapper";
+import { useResourceList } from "modules/state/hooks/useResourceList";
+import { useStores } from "modules/state/manager";
+import { ExternalLink } from "modules/ui/components/ExternalLink";
+import { Section } from "modules/ui/components/Section";
+import { VideoGrid } from "modules/video/components/VideoGrid";
 import React from "react";
-import WindowWidget from "components/WindowWidget";
-import { getOrg, fkOrg, fkVideoQuery } from "components/TS-API/API";
-import VideoList, { getLatestVideos } from "components/VideoList";
-import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
-import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  if (typeof context.query.orgID !== "string" || Number.isNaN(parseInt(context.query.orgID, 10))) {
-    throw new Error(`Invalid organization ID`);
-  }
+const Container = styled.div``;
 
-  const orgID = parseInt(context.query.orgID, 10);
+const Header = styled.div`
+  display: flex;
+`;
 
-  const orgData = await getOrg(orgID);
-  const latestVideos = await getLatestVideos(orgID);
+const PrimaryInfo = styled.div`
+  flex: 1;
+`;
 
-  return {
-    props: {
-      orgData,
-      latestVideos,
-    },
-  };
+const Title = styled.h1``;
+
+const Description = styled.div`
+  margin-top: 16px;
+
+  white-space: pre-wrap;
+  word-break: break-word;
+`;
+
+const SecondaryInfo = styled.div`
+  white-space: pre-wrap;
+  word-break: break-word;
+
+  display: flex;
+`;
+
+const InfoSection = styled(Section)`
+  margin-left: 32px;
+  min-width: 200px;
+`;
+
+const InfoSectionLine = styled.span`
+  display: block;
+  margin-bottom: 8px;
+`;
+
+const Content = styled.div`
+  margin-top: 16px;
+`;
+
+const LatestVideosHeading = styled.h2`
+  margin-bottom: 16px;
+`;
+
+type OrganizationViewProps = {
+  organization: Organization;
 };
 
-function OrganizationData(props: { orgData: fkOrg }): JSX.Element {
-  const { orgData } = props;
+function OrganizationView(props: OrganizationViewProps) {
+  const { videoStore } = useStores();
+
+  const { organization } = props;
+  const { name, description, postalAddress, streetAddress, editorName, editorEmail, editorMsisdn } = organization.data;
+
+  const videos = useResourceList(organization.videos, videoStore);
+
+  const renderDescription = () => {
+    if (!description) return null;
+
+    return <Description>{description}</Description>;
+  };
+
   return (
-    <div>
-      <h1>{orgData.name}</h1>
-      <p style={{ whiteSpace: "pre-line" }}>{orgData.description}</p>
-    </div>
+    <Container>
+      <Header>
+        <PrimaryInfo>
+          <Title>{name}</Title>
+          {renderDescription()}
+        </PrimaryInfo>
+        <SecondaryInfo>
+          <InfoSection icon="pencil" title="Redaktør">
+            <InfoSectionLine>{editorName}</InfoSectionLine>
+            <InfoSectionLine>
+              <ExternalLink href={`mailto:${editorEmail}`}>{editorEmail}</ExternalLink>
+            </InfoSectionLine>
+            <InfoSectionLine>
+              <ExternalLink href={`tel:${editorMsisdn}`}>{editorMsisdn}</ExternalLink>
+            </InfoSectionLine>
+          </InfoSection>
+          <InfoSection icon="mail" title="Postadresse">
+            {postalAddress.split("\n").map((t) => (
+              <InfoSectionLine key={t}>{t}</InfoSectionLine>
+            ))}
+          </InfoSection>
+          <InfoSection icon="home" title="Besøksadresse">
+            {streetAddress.split("\n").map((t) => (
+              <InfoSectionLine key={t}>{t}</InfoSectionLine>
+            ))}
+          </InfoSection>
+        </SecondaryInfo>
+      </Header>
+      <Content>
+        <LatestVideosHeading>Siste videoer</LatestVideosHeading>
+        <VideoGrid videos={videos} />
+        <ListTail list={organization.videos} />
+      </Content>
+    </Container>
   );
 }
 
-function OrganizationContactInfo(props: { orgData: fkOrg }): JSX.Element {
-  const { orgData } = props;
+const OrganizationPage = createResourcePageWrapper<Organization>({
+  getFetcher: (query, manager) => {
+    const { organizationStore } = manager.stores;
+    const { orgID } = query;
 
-  return (
-    <CardDeck>
-      <Card bg="light" className="text-dark">
-        <Card.Body>
-          <Card.Title>Redaktør</Card.Title>
-          <p>
-            {orgData.editorName}
-            <br />
-            <a href={`mailto:${orgData.editorEmail}`}>{orgData.editorEmail}</a>
-            <br />
-            <a href={`tel:${orgData.editorMsisdn}`}>{orgData.editorMsisdn}</a>
-          </p>
-        </Card.Body>
-      </Card>
-      <Card bg="light" className="text-dark">
-        <Card.Body>
-          <Card.Title>Postadresse</Card.Title>
-          <p style={{ whiteSpace: "pre" }}>{orgData.postalAddress}</p>
-        </Card.Body>
-      </Card>
-      <Card bg="light" className="text-dark">
-        <Card.Body>
-          <Card.Title>Besøksadresse</Card.Title>
-          <p style={{ whiteSpace: "pre" }}>{orgData.streetAddress}</p>
-        </Card.Body>
-      </Card>
-    </CardDeck>
-  );
-}
+    const safeOrgId = Number(orgID) ?? 0;
+    return organizationStore.fetchById(safeOrgId);
+  },
+  renderContent: (o) => <OrganizationView organization={o} />,
+});
 
-interface OrganizationPageProps {
-  orgData: fkOrg;
-  latestVideos: fkVideoQuery;
-}
-
-export default function OrganizationPage({ orgData, latestVideos }: OrganizationPageProps): JSX.Element {
-  return (
-    <>
-      <WindowWidget>
-        <OrganizationData orgData={orgData} />
-        <Card bg="light" className="text-dark">
-          <Card.Body>
-            <Card.Title>Siste videoer</Card.Title>
-            <VideoList videosJSON={latestVideos} />
-          </Card.Body>
-        </Card>
-        <p>&nbsp;</p>
-        <OrganizationContactInfo orgData={orgData} />
-      </WindowWidget>
-    </>
-  );
-}
+export default OrganizationPage;
