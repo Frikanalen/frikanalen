6258102c544c48f9ba50fc4c24d3e948a20633d3
Enitoni <enitoni@gmail.com>
2021-10-01 20:13:06 +0200

    add missing data to Organization

--- packages/frontend/modules/organization/resources/Organization.ts
+++ packages/frontend/modules/organization/resources/Organization.ts
@@ -3,6 +3,15 @@ import { Resource, ResourceFactory } from "modules/state/classes/Resource";
 export type OrganizationData = {
   id: number;
   name: string;
+  description: string;
+  postalAddress: string;
+  streetAddress: string;
+
+  // Temporary
+  editorId: number;
+  editorName: string;
+  editorEmail: string;
+  editorMsisdn: string;
 };
 
 export class Organization extends Resource<OrganizationData> {}
