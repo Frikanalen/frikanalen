24a920c5e6f79477602bbde2e3fc48cadb093905
Enitoni <enitoni@gmail.com>
2021-10-05 17:54:34 +0200

    add success state on forms and hide status after a while

--- packages/frontend/modules/form/hooks/useFormSubmission.ts
+++ packages/frontend/modules/form/hooks/useFormSubmission.ts
@@ -1,10 +1,13 @@
+import { wait } from "modules/lang/async";
 import { StatusType } from "modules/ui/components/StatusLine";
 import { useState } from "react";
 import { FieldsType, ObservableForm } from "../classes/ObservableForm";
 
+const statusTimeout = 5000;
+
 export const useFormSubmission = <F extends FieldsType>(
   form: ObservableForm<F>,
-  submit: (serialized: ObservableForm<F>["serialized"]) => Promise<void>,
+  submit: (serialized: ObservableForm<F>["serialized"]) => Promise<[StatusType, string] | void>,
   handleError: (e: any) => [StatusType, string] = () => ["error", "Noe gikk galt, prøv igjen senere"]
 ) => {
   const [status, setStatus] = useState<[StatusType, string]>(["info", ""]);
@@ -16,9 +19,20 @@ export const useFormSubmission = <F extends FieldsType>(
       setStatus(["info", "Vent litt..."]);
 
       try {
-        await submit(form.serialized);
+        const result = await submit(form.serialized);
+
+        if (result) {
+          setStatus(result);
+        } else {
+          setStatus(["success", "Skjemaet ble sendt"]);
+        }
+
+        await wait(statusTimeout);
+        setStatus(["info", ""]);
       } catch (e) {
         setStatus(handleError(e));
+        await wait(statusTimeout);
+        setStatus(["info", ""]);
       }
     } else {
       setStatus(["error", "Kontroller at skjemaet er gyldig"]);
