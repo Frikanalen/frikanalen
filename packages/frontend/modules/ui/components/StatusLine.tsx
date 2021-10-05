32b186274faef1504b7cca1f77ef94e29f149869
Enitoni <enitoni@gmail.com>
2021-10-05 17:53:43 +0200

    show icons and add success state in StatusLine

--- packages/frontend/modules/ui/components/StatusLine.tsx
+++ packages/frontend/modules/ui/components/StatusLine.tsx
@@ -1,14 +1,25 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { IconType } from "../types";
+import { SVGIcon } from "./SVGIcon";
 
 const Container = styled.span<{ type: StatusType }>`
+  display: flex;
+  align-items: center;
+
   font-size: 0.9em;
   font-weight: 600;
 
   ${(props) => {
     if (props.type === "error") {
       return css`
-        color: ${props.theme.color.secondAccent};
+        color: ${props.theme.stateColor.danger};
+      `;
+    }
+
+    if (props.type === "success") {
+      return css`
+        color: ${props.theme.stateColor.success};
       `;
     }
 
@@ -18,7 +29,20 @@ const Container = styled.span<{ type: StatusType }>`
   }}
 `;
 
-export type StatusType = "info" | "error";
+const Icon = styled(SVGIcon)`
+  width: 24px;
+  height: 24px;
+
+  margin-right: 8px;
+`;
+
+const typeToIconMap: Record<StatusType, IconType | undefined> = {
+  info: undefined,
+  success: "circledCheckmark",
+  error: "triangularExclamation",
+};
+
+export type StatusType = "info" | "success" | "error";
 
 export type StatusLineProps = {
   type: StatusType;
@@ -28,9 +52,17 @@ export type StatusLineProps = {
 
 export function StatusLine(props: StatusLineProps) {
   const { type, className, message } = props;
+  const icon = typeToIconMap[type];
+
+  const renderIcon = () => {
+    if (!icon) return null;
+
+    return <Icon name={icon} />;
+  };
 
   return (
     <Container className={className} type={type}>
+      {renderIcon()}
       {message}
     </Container>
   );
