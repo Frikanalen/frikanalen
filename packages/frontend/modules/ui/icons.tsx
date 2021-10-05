9f91d6caf6996e3609b6559203a0fdc6489dfd5d
Enitoni <enitoni@gmail.com>
2021-10-05 17:53:15 +0200

    add circledCheckmark and triangularExclamation icon

--- packages/frontend/modules/ui/icons.tsx
+++ packages/frontend/modules/ui/icons.tsx
@@ -91,3 +91,25 @@ export const home = (
     />
   </svg>
 );
+
+export const circledCheckmark = (
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
+
+export const triangularExclamation = (
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+    />
+  </svg>
+);
