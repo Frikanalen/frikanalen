32f0a7e4615498a9f662313bfe1481ff37a952ad
Enitoni <enitoni@gmail.com>
2021-10-10 18:19:08 +0200

    Tweak GenericButton label font size

--- packages/frontend/modules/ui/components/GenericButton.tsx
+++ packages/frontend/modules/ui/components/GenericButton.tsx
@@ -64,6 +64,7 @@ const Container = styled(Button as ButtonWithProps<{ variant: GenericButtonVaria
 
 const Label = styled.span`
   font-weight: 500;
+  font-size: 0.9em;
 `;
 
 export type GenericButtonProps = ButtonProps & {
