12039bde87476c2ca85c42e789e19625dceedc5d
Enitoni <enitoni@gmail.com>
2021-10-10 19:55:13 +0200

    Add camera icon

--- packages/frontend/modules/ui/icons.tsx
+++ packages/frontend/modules/ui/icons.tsx
@@ -135,3 +135,14 @@ export const film = (
     />
   </svg>
 );
+
+export const camera = (
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+    />
+  </svg>
+);
