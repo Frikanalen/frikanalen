ca316e8b15a64a4b072615f43a9057f121e08aa4
Enitoni <enitoni@gmail.com>
2021-10-12 22:20:25 +0200

    add menu icon

--- packages/frontend/modules/ui/icons.tsx
+++ packages/frontend/modules/ui/icons.tsx
@@ -174,3 +174,9 @@ export const fire = (
     />
   </svg>
 );
+
+export const menu = (
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+  </svg>
+);
