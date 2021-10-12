eb2d2d7aac954741e0a116b7a3a94ef11607c8a3
Enitoni <enitoni@gmail.com>
2021-10-12 17:53:19 +0200

    add empty state to VideoGrid

--- packages/frontend/modules/video/components/VideoGrid.tsx
+++ packages/frontend/modules/video/components/VideoGrid.tsx
@@ -1,8 +1,11 @@
 import styled from "@emotion/styled";
+import { EmptyState } from "modules/ui/components/EmptyState";
 import { Video } from "../resources/Video";
 import { VideoGridItem } from "./VideoGridItem";
 
-const Container = styled.ul`
+const Container = styled.div``;
+
+const Grid = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(min(300px, 100%), 1fr));
   gap: 16px;
@@ -15,11 +18,20 @@ export type VideoGridProps = {
 export function VideoGrid(props: VideoGridProps) {
   const { videos } = props;
 
+  const renderEmptyState = () => {
+    if (videos.length > 0) return null;
+
+    return <EmptyState title="Ingen videoer enda" icon="film" />;
+  };
+
   return (
     <Container>
-      {videos.map((v) => (
-        <VideoGridItem key={v.data.id} video={v} />
-      ))}
+      {renderEmptyState()}
+      <Grid>
+        {videos.map((v) => (
+          <VideoGridItem key={v.data.id} video={v} />
+        ))}
+      </Grid>
     </Container>
   );
 }
