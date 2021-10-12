5fa962ccccb736c14fb587ab8bfdfec40cef3b5d
Enitoni <enitoni@gmail.com>
2021-10-12 16:27:12 +0200

    prevent artificial delay on server

--- packages/frontend/modules/core/constants.ts
+++ packages/frontend/modules/core/constants.ts
@@ -1,3 +1,3 @@
 export const CONTENT_WIDTH = 1500;
 export const IS_SERVER = typeof window === "undefined";
-export const ARTIFICIAL_DELAY = 500;
+export const ARTIFICIAL_DELAY = IS_SERVER ? 0 : 500;
