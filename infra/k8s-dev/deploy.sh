530deed814f963aa7ecada85cbae699d6cd2c6d5
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-10-13 23:21:26 +0200

    Improve deploy script

--- infra/k8s-dev/deploy.sh
+++ infra/k8s-dev/deploy.sh
@@ -18,7 +18,6 @@ echo "Opening ingresses..."
 
 $k apply -f ingress.yaml
 
-
 echo "Adding cluster-internal DNS alias for fk.dev.local->ingress.traefik"
 
 $k apply -f coredns-config.yaml
@@ -27,25 +26,33 @@ echo "Creating backend namespace"
 
 $k apply -f $BASEPATH/database/000-namespace.yaml
 
-echo "Creating Django SECRET_KEY"
+if ! $k get secret django-secret-key 2> /dev/null; then
+    echo "Creating Django SECRET_KEY"
 
-SECRET_KEY=$(base64 /dev/urandom | head -c50)
+    SECRET_KEY=$(base64 /dev/urandom | head -c50)
 
-$k create secret generic django-secret-key \
-    --from-literal=SECRET_KEY=${SECRET_KEY}
+    $k create secret generic django-secret-key \
+        --from-literal=SECRET_KEY=${SECRET_KEY}
+else
+    echo "Django SECRET_KEY already exists, not generating..."
+fi;
 
-echo "Creating database/backend secrets"
+if ! $k get secret database-api-secret 2> /dev/null; then
+    echo "Creating database/backend secrets"
 
-POSTGRES_REPLICATION_PASSWORD=$(pwgen 32 1)
-POSTGRES_PASSWORD=$(pwgen 32 1)
-DATABASE_URL="postgres://fkweb:${POSTGRES_PASSWORD}@database-api/fkweb"
-FKSCHEDULE_PASSWORD=$(pwgen 32 1)
+    POSTGRES_REPLICATION_PASSWORD=$(pwgen 32 1)
+    POSTGRES_PASSWORD=$(pwgen 32 1)
+    DATABASE_URL="postgres://fkweb:${POSTGRES_PASSWORD}@database-api/fkweb"
+    FKSCHEDULE_PASSWORD=$(pwgen 32 1)
 
-$k create secret generic database-api-secret \
-    --from-literal=POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-    --from-literal=DATABASE_URL=${DATABASE_URL} \
-    --from-literal=FKSCHEDULE_PASSWORD=${FKSCHEDULE_PASSWORD} \
-    --from-literal=POSTGRES_REPLICATION_PASSWORD=${POSTGRES_REPLICATION_PASSWORD}
+    $k create secret generic database-api-secret \
+        --from-literal=POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+        --from-literal=DATABASE_URL=${DATABASE_URL} \
+        --from-literal=FKSCHEDULE_PASSWORD=${FKSCHEDULE_PASSWORD} \
+        --from-literal=POSTGRES_REPLICATION_PASSWORD=${POSTGRES_REPLICATION_PASSWORD}
+else
+    echo "Database secrets already exist, not generating..."
+fi;
 
 echo "Applying database CRDs..."
 
@@ -57,46 +64,45 @@ echo "Starting backend..."
 $k apply -f $BASEPATH/django/django.yaml
 
 echo "Waiting for Django to come up"
-sleep 20
+until curl -fs http://fk.dev.local/api> /dev/null; do sleep 1; echo -n .; done
 
-echo "Creating upload superuser"
+if ! $k get secret upload-receiver 2> /dev/null; then
+    echo "Creating upload superuser"
 
-DJANGO_POD=$($k get pods --selector=app=django --output=jsonpath={.items..metadata.name})
-DSP=$(pwgen 32 1)
-$k exec -ti $DJANGO_POD -c django -- DJANGO_SUPERUSER_PASSWORD=${DSP} env ./manage.py createsuperuser --date_of_birth 1970-01-01 --email upload@frikanalen.no --no-input
+    DJANGO_POD=$($k get pods --selector=app=django --output=jsonpath={.items..metadata.name})
+    DSP=$(pwgen 32 1)
+    $k exec -ti $DJANGO_POD -c django -- DJANGO_SUPERUSER_PASSWORD=${DSP} env ./manage.py createsuperuser --date_of_birth 1970-01-01 --email upload@frikanalen.no --no-input
 
-echo "Obtaining token"
+    echo "Obtaining token"
 
-FK_TOKEN=$(curl -u upload@frikanalen.no:${DSP} http://fk.dev.local/api/obtain-token | jq -r .key)
+    FK_TOKEN=$(curl -u upload@frikanalen.no:${DSP} http://fk.dev.local/api/obtain-token | jq -r .key)
 
-echo "Writing upload-service secrets"
+    echo "Writing upload-service secrets"
 
-# These are statically defined by s3-ninja, afaict
-AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    # These are statically defined by s3-ninja, afaict
+    AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
+    AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
-$k create secret -n default generic upload-receiver \
-    --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
-    --from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
-    --from-literal=AWS_REGION=no-where-1 \
-    --from-literal=FK_TOKEN=${FK_TOKEN}
+    $k create secret -n default generic upload-receiver \
+        --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+        --from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+        --from-literal=AWS_REGION=no-where-1 \
+        --from-literal=FK_TOKEN=${FK_TOKEN}
+
+else
+    echo "Skipping upload receiver configuration"
+fi;
+
+echo "Setting up Kafka..."
+$k apply -f zookeeper.yaml
 
 echo "Starting upload-receiver service"
 
 # TODO: Move environment into config map
-$k apply -f ingest/upload-receiver/upload-receiver.yaml
-
-
-echo "Setting up database for APIv2"
-
-POSTGRES_PASSWORD=$(pwgen 32 1)
-POSTGRES_REPLICATION_PASSWORD=$(pwgen 32 1)
-APIV2_PASSWORD=$(pwgen 32 1)
-
-$k create secret generic database-api-v2-secret \
-    --from-literal=POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-    --from-literal=POSTGRES_REPLICATION_PASSWORD=${POSTGRES_REPLICATION_PASSWORD} \
-    --from-literal=APIV2_PASSWORD=${APIV2_PASSWORD}
+$k apply -f $BASEPATH/ingest/upload-receiver/upload-receiver.yaml
 
 #$k apply -f $BASEPATH/django/periodic-schedule-tasks/001-fill_agenda_with_jukebox.yaml
 #$k apply -f $BASEPATH/django/periodic-schedule-tasks/001-fill_agenda.yaml
+
+echo "Starting S3 emulation service"
+$k apply -f s3-emulator.yaml
