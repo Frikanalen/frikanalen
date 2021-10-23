b114ef9ca8ced152af7db36725ce38fded9d790e
Enitoni <enitoni@gmail.com>
2021-10-23 23:16:20 +0200

    Implement the end of times

--- packages/frontend/modules/network/constants.ts
+++ packages/frontend/modules/network/constants.ts
@@ -1,3 +1,4 @@
 export const UPLOAD_RETRY_COUNT = 5;
 export const TUS_RESUMABLE = "1.0.0";
 export const TUS_CHUNK_SIZE = 10 * 1024 * 1024; // 10mib
+export const THE_END_OF_TIMES = "Fri, 31 Dec 9999 23:59:59 GMT";
