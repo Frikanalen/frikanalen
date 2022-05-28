dd5c3ccf8a1173c46a99ed73664d6aef9ffc81fe
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-05-28 17:20:53 +0200

    add beta.frikanalen.no CRDs

---
+++ infra/k8s-beta/README.md
@@ -0,0 +1,1 @@
+This contains the CRDs for beta.frikanalen.no.
--- infra/k8s-beta/makeSecrets.bash
+++ infra/k8s-beta/makeSecrets.bash
@@ -5,3 +5,6 @@ POSTGRES_REPLICATION_PASSWORD=$(pwgen 30 1)
 DATABASE_URL=postgres://fk:${POSTGRES_PASSWORD}@postgres/fk
 
 kubectl create -n beta secret generic database --from-literal=DATABASE_URL=${DATABASE_URL} --from-literal=POSTGRES_PASSWORD=${POSTGRES_PASSWORD} --from-literal=POSTGRES_REPLICATION_PASSWORD=${POSTGRES_REPLICATION_PASSWORD}
+
+FK_API_KEY=$(pwgen 30 1)
+kubectl create -n beta secret generic fk-api-key --from-literal=FK_API_KEY=${FK_API_KEY}
