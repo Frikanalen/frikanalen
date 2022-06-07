628c28b068f217510523e90c9cf090ce92ad91ca
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-06-07 17:57:55 +0200

    initialize multiviewer on startup

--- packages/utils/atem-control/src/atem/utils.ts
+++ packages/utils/atem-control/src/atem/utils.ts
@@ -10,11 +10,18 @@ import { MULTI_VIEWER_INPUT } from "../server";
 export const applyInitialConfiguration = async (atem: Atem) => {
   const afv = AudioMixOption.AudioFollowVideo;
 
+    console.log("HIII")
+    await atem.setMultiViewerSource({source: 1, windowIndex: 2});
+    await atem.setMultiViewerSource({source: 2, windowIndex: 3});
+    await atem.setMultiViewerSource({source: 3, windowIndex: 4});
+    await atem.setMultiViewerSource({source: 4, windowIndex: 5});
+    await atem.setMultiViewerSource({source: 5, windowIndex: 6});
   try {
     await atem.setAudioMixerInputProps(1, { mixOption: afv });
     await atem.setAudioMixerInputProps(2, { mixOption: afv });
     await atem.setAudioMixerInputProps(3, { mixOption: afv });
     await atem.setAudioMixerInputProps(4, { mixOption: afv });
+
   } catch (e) {
     console.log("got exception while setting audio mixer input props");
     console.log(e);
